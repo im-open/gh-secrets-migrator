c61e60e7cfa67898abb8c1e6d1c48b3c1ecf02b8
Renan Alm <xrenan.alm@gmail.com>
2025-11-18 14:56:19 +0100

    refactoring - clean up

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 """GitHub Secrets Migrator - Migrate secrets from one repository to another."""
 
+import warnings
+
+# Suppress urllib3 SSL warnings
+warnings.filterwarnings("ignore", message="Unverified HTTPS request")
+warnings.filterwarnings("ignore", module="urllib3")
+
 from src.cli import migrate
 
 if __name__ == "__main__":
