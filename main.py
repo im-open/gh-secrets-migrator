d75dc972ae2072181c69f8967f255a255be0aafa
Renan Alm <xrenan.alm@gmail.com>
2025-11-18 14:58:08 +0100

    fix linter

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 
 import warnings
 
+from src.cli import migrate
+
 # Suppress urllib3 SSL warnings
 warnings.filterwarnings("ignore", message="Unverified HTTPS request")
 warnings.filterwarnings("ignore", module="urllib3")
-
-from src.cli import migrate
 
 if __name__ == "__main__":
     migrate()
